924d5680eeecd476b96cad965c423f349f476ff0
JOSE JOAO CAETANO <josejoaocaetano16@gmail.com>
2025-05-02 22:15:38 -0300

    Add files via upload

---
+++ double.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+double x;
+// Atribuindo
+x = 9;
+x = 3.14;
+// Declarando mais de uma variável
+double y, z;
+// Imprimindo
+printf("%lf", x);
+// Lendo
+scanf("%lf", &x);
+}
